33c67009325fa2c83af811dc5b63969fd248b685
GoAmeer030 <59198817+GoAmeer030@users.noreply.github.com>
2021-10-13 19:58:54 +0530

    Update settings.py

--- danker_man/settings.py
+++ danker_man/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     "mainapp/static",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
