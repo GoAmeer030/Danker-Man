595555f89140dc024cc4589bf2778372e10b6fb0
GoAmeer030 <ameer03062003@gmail.com>
2021-10-20 16:47:54 +0530

    Heroku tweeks

--- danker_man/settings.py
+++ danker_man/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     BASE_DIR,
     "mainapp/static",
